458cfc832944dc3fa47e90c38158015e5746529a
Andy Yang <yangj079@newschool.edu>
2018-04-17 15:42:12 -0400

    p

---
+++ animations/animation.js
@@ -0,0 +1,19 @@
+(function(document) {
+    
+    $(document).ready(function(){
+    console.log("document ready");
+    
+    setTimeout(() => {
+        $('#hello-text').addClass('animated zoomInDown Infinite');
+    }, 500);
+    
+  });  
+    
+    
+
+
+
+
+})(document);
+
+
